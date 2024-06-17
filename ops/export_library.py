3688a15953ad1718bcab72903dc70e4ad5ae59e3
zachEastin <zaqypoo@gmail.com>
2024-06-17 17:36:36 -0500

    CHANGE: Parallelize compression

--- ops/export_library.py
+++ ops/export_library.py
@@ -2,13 +2,14 @@ from typing import TYPE_CHECKING
 import zipfile
 from importlib.util import find_spec
 from pathlib import Path
-from time import time
+import time
 
 import bpy
 from bpy.types import Operator, UserAssetLibrary, Context
 from bpy.props import EnumProperty, IntProperty
 from bpy_extras import asset_utils
 from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 
 from .. import utils
 
@@ -34,6 +35,76 @@ if find_spec("lzma"):
         "ZIP_LZMA", "Ultra",
         "Compress files using LZMA compression algorithm. This is the slowest option but gives the best compression ratio. NOT RECOMMENDED as not all OS's include this library when extracting/importing(ZIP_LZMA)"
     ))
+
+class EmptyCompressor(object):
+    def flush(self):
+        return bytes(0)
+class ZipFileParallel(zipfile.ZipFile):
+    """
+    Example
+    -------
+    ```py
+    file = ZipFileParallel('file.zip','w',zipfile.ZIP_BZIP2)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+
+        for img in enumerate(images):
+            fname = f'images/{idx}.raw'
+            futures.append(executor.submit(file.writestr, fname ,img.tobytes()))
+        
+        concurrent.futures.wait(futures)
+    ```
+    """
+    def writestr(self, zinfo_or_arcname, data,
+                 compress_type=None, compresslevel=None):
+        """Write a file into the archive.  The contents is 'data', which
+        may be either a 'str' or a 'bytes' instance; if it is a 'str',
+        it is encoded as UTF-8 first.
+        'zinfo_or_arcname' is either a ZipInfo instance or
+        the name of the file in the archive.
+        """
+        if isinstance(data, str):
+            data = data.encode("utf-8")
+        if not isinstance(zinfo_or_arcname, zipfile.ZipInfo):
+            zinfo = zipfile.ZipInfo(filename=zinfo_or_arcname,
+                                    date_time=time.localtime(time.time())[:6])
+            zinfo.compress_type = self.compression
+            zinfo._compresslevel = self.compresslevel
+            if zinfo.filename[-1] == '/':
+                zinfo.external_attr = 0o40775 << 16  # drwxrwxr-x
+                zinfo.external_attr |= 0x10  # MS-DOS directory flag
+            else:
+                zinfo.external_attr = 0o600 << 16  # ?rw-------
+        else:
+            zinfo = zinfo_or_arcname
+
+        if not self.fp:
+            raise ValueError(
+                "Attempt to write to ZIP archive that was already closed")
+
+        if compress_type is not None:
+            zinfo.compress_type = compress_type
+
+        if compresslevel is not None:
+            zinfo._compresslevel = compresslevel
+
+        zinfo.file_size = len(data)  # Uncompressed size
+        crc = zipfile.crc32(data, 0)
+        # compress data
+        compressor = zipfile._get_compressor(
+            zinfo.compress_type, zinfo._compresslevel)
+        if compressor and hasattr(compressor, 'compress'):
+            data = compressor.compress(data)
+            data += compressor.flush()
+
+        with self._lock:
+            with self.open(zinfo, mode='w') as dest:
+                dest._compressor = None  # remove the compressor so it doesn't compress again
+                dest.write(data)
+                dest._crc = crc
+                dest._file_size = zinfo.file_size
+                dest._compress_size = len(data)
+                dest._compressor = EmptyCompressor()  # use an empty compressor
 
 
 class SH_OT_ExportLibrary(Operator):
@@ -128,14 +199,17 @@ class SH_OT_ExportLibrary(Operator):
         scene_sets: scene.SH_Scene = context.scene.superhive
         self.prog = scene_sets.header_progress_bar
         self.zip_path = Path(f"{self.lib.path}.zip")
+        self.z = ZipFileParallel(
+            self.zip_path, "w",
+            getattr(zipfile, self.compression_type),
+            compresslevel=self.deflated_compression_level if self.compression_type == "ZIP_DEFLATED" else self.bzip_compression_level
+        )
 
         # Setup export thread
-        self._th = Thread(target=self.zip_files)
+        self._th = Thread(target=self.zip_files_parallel)
         context.window_manager.modal_handler_add(self)
         self._timer = context.window_manager.event_timer_add(0.1, window=context.window)
-        self.st = time()
         self.prog.start()
-        self.last_time = time()
         self._th.start()
 
         return {"RUNNING_MODAL"}
@@ -149,20 +223,12 @@ class SH_OT_ExportLibrary(Operator):
         self.prog.update_formated_time()
 
         if (event.type == "ESC" and event.mouse_region_x > 0 and event.mouse_region_y > 0) or self.prog.cancel:
+            self.prog.cancel = True
             self.finished(context)
             return {"CANCELLED"}
         elif not self._th or not self._th.is_alive():
             self.finished(context)
             return {"FINISHED"}
-
-        # Update progress bar every 10 seconds
-        # if time() - self.last_time >= 0.01:
-        #     self.prog.progress += 1
-        #     self.prog.update_formated_time()
-        #     self.last_time = time()
-        # if self.prog.progress >= 100:
-        #     self.finished(context)
-        #     return {"FINISHED"}
 
         return {"RUNNING_MODAL"}
 
@@ -178,12 +244,34 @@ class SH_OT_ExportLibrary(Operator):
 
     def finished(self, context: Context) -> None:
         self._th.join()
+        self.z.close()
         context.window_manager.event_timer_remove(self._timer)
         self.prog.end()
         try:
             utils.open_location(str(self.zip_path))
         except Exception as e:
             print("Unable to open export folder:", e)
+
+    def zip_files_parallel(self) -> None:
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            for file in self.files:
+                future = executor.submit(self._compress_file_auto_split, file)
+                futures.append(future)
+            # wait for all threads to finish and report errors
+            for future in futures:
+                try:
+                    future.result()
+                except Exception as e:
+                    print(f"Error compressing file: {e}")
+
+    def _compress_file_auto_split(self, file: Path) -> None:
+        if self.prog.cancel:
+            return
+
+        self.z.writestr(str(file.relative_to(self.directory)), file.read_bytes())
+        self.files_written += 1
+        self.updated = True
 
 
 classes = (
