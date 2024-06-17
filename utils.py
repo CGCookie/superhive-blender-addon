49175aedbc947f2f8362cbe345c7038bdabd5d7d
zachEastin <zaqypoo@gmail.com>
2024-06-17 14:15:11 -0500

    add categories/subcategories documentation

--- utils.py
+++ utils.py
@@ -12,9 +12,30 @@ class Catalog:
         id: str = None,
         path: str = None,
     ) -> None:
+        """
+        Initialize a Catalog object.
+
+        Parameters
+        ----------
+        data : Union["CatalogsFile", "Catalog"]
+            The parent Catalog or CatalogsFile object.
+        name : str
+            The name or `simple_name` of the Catalog object. Will set `simple_name` and `name` if `path` is not provided.
+        id : str, optional
+            The unique identifier for the Catalog object. If not provided, a new UUID (uuid4) will be generated.
+        path : str, optional
+            The path associated with the Catalog object. If provided, the name of the Catalog object will be extracted from the path.
+        
+        Returns
+        -------
+        None
+        """
         self.data = data
+        """The parent object"""
         self.id = id or str(uuid.uuid4())
+        """The unique identifier for the catalog"""
         self.path = path
+        """The path to the catalog's greatest parent catalog"""
         self.simple_name = name
         if path:
             self.name = path.split("/")[-1]
@@ -22,20 +43,55 @@ class Catalog:
             self.name = name.split("-")[-1]
 
         self.children: dict[str, Catalog] = {}
+        """Sub-catalogs of this catalog"""
 
         if not self.path:
             self.load_path()
 
     def add_child(self, name: str, id: str = None, path: str = None) -> "Catalog":
+        """Add a child catalog to this catalog.
+
+        Parameters
+        ----------
+        name : str
+            The name or `simple_name` of the catalog.
+        id : str, optional
+            The `uuid4` of an existing catalog you want to retain, by default None
+        path : str, optional
+            The verbal path from the base catalog to this catalog, by default None
+
+        Returns
+        -------
+        Catalog
+            The child catalog that was added.
+        """
         self.children[id] = Catalog(self, name, id=id, path=path or self.load_path())
         return self.children[id]
 
     def remove_child(self, id: str) -> None:
+        """
+        Removes a child with the specified ID from the list of children.
+
+        Parameters
+        ----------
+        id : str
+            The UUID of the child to be removed.
+
+        Returns
+        -------
+        None
+        """
         if id in self.children:
             del self.children[id]
 
     def load_path(self) -> None:
-        """Figure out the catalog's path. Will also set the simple_name"""
+        """
+        Load the path of the current data object.
+        Will also set `simple_name`
+
+        Returns:
+            None
+        """
         data = self
         path = []
         while not isinstance(data, CatalogsFile):
@@ -52,25 +108,52 @@ class Catalog:
         return lines
 
     def get_line(self) -> str:
-        """Get the line to write to the catalogs file for this catalog."""
-        return f"{self.id}:{self.path}:{self.simple_name}\n"
+            """
+            Returns a formatted string representing the line information.
+
+            Returns:
+                str: A string in the format "{id}:{path}:{simple_name}\n".
+            """
+            return f"{self.id}:{self.path}:{self.simple_name}\n"
 
     def print_catalog_tree(self, indent=0) -> None:
-        """Print the catalog tree to the console."""
+        """
+        Print the catalog tree starting from the current catalog.
+
+        Parameters
+        ----------
+        indent : int, optional
+            The number of indentations to apply to the printed tree (default is 0).
+
+        Returns
+        -------
+        None
+            This method does not return anything.
+        """
         ind = "  " * indent * 4
         print(f"{ind}{self.name} ({self.id})")
         for catalog in self.children.values():
             catalog.print_catalog_tree(indent + 1)
 
     def find_catalog(self, id: str) -> "Catalog":
-        cat = self.children.get(id)
-        if cat:
-            return cat
+            """
+            Find a catalog with the given ID among this catalog's children.
 
-        for catalog in self.children.values():
-            cat = catalog.find_catalog(id)
+            Parameters:
+            - id (str): The ID of the catalog to find.
+
+            Returns:
+            - Catalog: The catalog with the given ID, if found. Otherwise, None.
+            """
+
+            cat = self.children.get(id)
             if cat:
                 return cat
+
+            for catalog in self.children.values():
+                cat = catalog.find_catalog(id)
+                if cat:
+                    return cat
 
     def generate_id_path(self) -> list[tuple[str, str]]:
         """Generate the path of ids to this catalog.
@@ -87,6 +170,37 @@ class Catalog:
         return list(reversed(path))
 
     def get_catalog_by_path(self, path_parts: list[str]) -> "Catalog":
+        """
+        Get a catalog by its path.
+
+        Parameters
+        ----------
+        path_parts : list[str]
+            The list of path parts representing the catalog's path.
+
+        Returns
+        -------
+        Catalog
+            The catalog object found by the given path.
+
+        Notes
+        -----
+        This method recursively searches for a catalog by its path in the children catalogs.
+
+        If the `path_parts` list contains only one element and it matches a child catalog's name, that child catalog is returned.
+
+        If the `path_parts` list contains more than one element, the method recursively calls itself with the remaining path parts,
+        searching for the catalog in the children catalogs of the current catalog.
+
+        If a catalog is found, it is returned. Otherwise, None is returned.
+
+        Examples
+        --------
+        >>> path_parts = ["parent", "child", "grandchild"]
+        >>> catalog = get_catalog_by_path(path_parts)
+        >>> print(catalog)
+        <Catalog object at 0x7f8a9c6a7a90>
+        """
         if len(path_parts) == 1 and path_parts[0] in self.children:
             return self.children.get(path_parts[0])
 
@@ -96,7 +210,16 @@ class Catalog:
                 return cat
 
     def ensure_correct_child_parenting(self) -> None:
-        """Ensure that the children of this catalog have the correct parent."""
+        """
+        Ensure correct parenting of child catalogs.
+
+        This method iterates over the children catalogs and sets the parent catalog as the data attribute for each child.
+        It then recursively calls `ensure_correct_child_parenting` on each child catalog to ensure correct parenting
+        throughout the hierarchy.
+
+        Returns:
+            None
+        """
         for catalog in self.children.values():
             catalog.data = self
             catalog.ensure_correct_child_parenting()
@@ -170,7 +293,35 @@ class CatalogsFile:
     def add_catalog(
         self, name: str, id: str = None, path: str = None, auto_place=False
     ) -> Catalog:
-        """Add a catalog"""
+        """
+        Add a catalog at the root level or under a parent catalog.
+
+        Parameters
+        ----------
+        name : str
+            The name of the catalog.
+        id : str, optional
+            The ID of the catalog. If not provided, a UUID will be generated.
+        path : str, optional
+            The path of the catalog. If not provided, the catalog will be added at the root level.
+        auto_place : bool, optional
+            If True and `path` is provided, the catalog will be automatically placed under the specified path
+            if a parent catalog exists. If False, the catalog will be added at the root level.
+
+        Returns
+        -------
+        Catalog
+            The added catalog.
+
+        Notes
+        -----
+        - If `auto_place` is True and `path` is provided, the method will attempt to find the parent catalog
+          based on the provided path. If a parent catalog is found, the new catalog will be added as a child
+          of the parent catalog. If no parent catalog is found, the method will recursively call itself with
+          `auto_place` set to False to add the catalog at the root level.
+        - If `auto_place` is False or `path` is not provided, the method will add the catalog at the root level.
+
+        """
         if auto_place and path:
             parent_path = path.split("/")
             if len(parent_path) > 1:
@@ -203,7 +354,7 @@ class CatalogsFile:
         return a
 
     def write_file(self) -> None:
-        """Write the catalogs to the catalogs file."""
+        """Write the catalogs to the blender_assets.cats.txt file."""
         with open(self.path, "w") as file:
             file.write(self.initial_text + "\n\n")
             file.write(f"VERSION {self.VERSION}\n\n")
@@ -218,16 +369,42 @@ class CatalogsFile:
             catalog.print_catalog_tree()
 
     def find_catalog(self, id: str) -> Catalog:
-        cat = self.catalogs.get(id)
-        if cat:
-            return cat
+            """
+            Find a catalog by its ID.
 
-        for catalog in self.catalogs.values():
-            cat = catalog.find_catalog(id)
+            Parameters
+            ----------
+            id : str
+                The ID of the catalog to find.
+
+            Returns
+            -------
+            Catalog
+                The found catalog, or None if not found.
+            """
+            cat = self.catalogs.get(id)
             if cat:
                 return cat
 
+            for catalog in self.catalogs.values():
+                cat = catalog.find_catalog(id)
+                if cat:
+                    return cat
+
     def get_catalog_by_path(self, path: str) -> Catalog:
+        """
+        Get a catalog by its path.
+
+        Parameters
+        ----------
+        path : str
+            The path of the catalog.
+
+        Returns
+        -------
+        Catalog
+            The catalog object if found, None otherwise.
+        """
         path_parts = path.split("/")
         if len(path_parts) == 1:
             return self.catalogs.get(path)
@@ -238,7 +415,18 @@ class CatalogsFile:
                 return cat
 
     def add_catalog_from_other(self, catalog: Catalog) -> None:
-        """Add a catalog from another CatalogsFile"""
+        """
+        Add a catalog from another catalog.
+
+        Parameters
+        ----------
+        catalog : Catalog
+            The catalog to be added.
+
+        Returns
+        -------
+        None
+        """
         id_path = catalog.generate_id_path()
 
         id, simple_name = id_path.pop(0)
