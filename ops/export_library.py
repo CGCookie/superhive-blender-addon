ae8e31d510c28229305e17da24deecfa3fce8c8b
zachEastin <14118550+zachEastin@users.noreply.github.com>
2024-08-15 15:34:35 -0500

    NEW: Export thumbnails with zip

--- ops/export_library.py
+++ ops/export_library.py
@@ -17,24 +17,26 @@ from . import polls
 if TYPE_CHECKING:
     from ..settings import scene
 
-comp_enum = [(
-    "ZIP_STORED", "None",
-    "No compression. Just place files in .zip file. This is the fastest option. (ZIP_STORED)"
-)]
+comp_enum = [
+    ("ZIP_STORED", "None", "No compression. Just place files in .zip file. This is the fastest option. (ZIP_STORED)")
+]
 if find_spec("zlib"):
     comp_enum.append((
-        "ZIP_DEFLATED", "Standard",
-        "Compress files using zlib, ie the normal zip compression algorithm. This option is slow with not the best compression. (ZIP_DEFLATED)"
+        "ZIP_DEFLATED",
+        "Standard",
+        "Compress files using zlib, ie the normal zip compression algorithm. This option is slow with not the best compression. (ZIP_DEFLATED)",
     ))
 if find_spec("bz2"):
     comp_enum.append((
-        "ZIP_BZIP2", "Maximum",
-        "Compress files using bzip2 compression algorithm. This is a good balance between speed and compression ratio. (ZIP_BZIP2)"
+        "ZIP_BZIP2",
+        "Maximum",
+        "Compress files using bzip2 compression algorithm. This is a good balance between speed and compression ratio. (ZIP_BZIP2)",
     ))
 if find_spec("lzma"):
     comp_enum.append((
-        "ZIP_LZMA", "Ultra",
-        "Compress files using LZMA compression algorithm. This is the slowest option but gives the best compression ratio. NOT RECOMMENDED as not all OS's include this library when extracting/importing(ZIP_LZMA)"
+        "ZIP_LZMA",
+        "Ultra",
+        "Compress files using LZMA compression algorithm. This is the slowest option but gives the best compression ratio. NOT RECOMMENDED as not all OS's include this library when extracting/importing(ZIP_LZMA)",
     ))
 
 
@@ -55,12 +57,12 @@ class ZipFileParallel(zipfile.ZipFile):
         for img in enumerate(images):
             fname = f'images/{idx}.raw'
             futures.append(executor.submit(file.writestr, fname ,img.tobytes()))
-        
+
         concurrent.futures.wait(futures)
     ```
     """
-    def writestr(self, zinfo_or_arcname, data,
-                 compress_type=None, compresslevel=None):
+
+    def writestr(self, zinfo_or_arcname, data, compress_type=None, compresslevel=None):
         """Write a file into the archive.  The contents is 'data', which
         may be either a 'str' or a 'bytes' instance; if it is a 'str',
         it is encoded as UTF-8 first.
@@ -70,11 +72,10 @@ class ZipFileParallel(zipfile.ZipFile):
         if isinstance(data, str):
             data = data.encode("utf-8")
         if not isinstance(zinfo_or_arcname, zipfile.ZipInfo):
-            zinfo = zipfile.ZipInfo(filename=zinfo_or_arcname,
-                                    date_time=time.localtime(time.time())[:6])
+            zinfo = zipfile.ZipInfo(filename=zinfo_or_arcname, date_time=time.localtime(time.time())[:6])
             zinfo.compress_type = self.compression
             zinfo._compresslevel = self.compresslevel
-            if zinfo.filename[-1] == '/':
+            if zinfo.filename[-1] == "/":
                 zinfo.external_attr = 0o40775 << 16  # drwxrwxr-x
                 zinfo.external_attr |= 0x10  # MS-DOS directory flag
             else:
@@ -83,8 +84,7 @@ class ZipFileParallel(zipfile.ZipFile):
             zinfo = zinfo_or_arcname
 
         if not self.fp:
-            raise ValueError(
-                "Attempt to write to ZIP archive that was already closed")
+            raise ValueError("Attempt to write to ZIP archive that was already closed")
 
         if compress_type is not None:
             zinfo.compress_type = compress_type
@@ -95,14 +95,13 @@ class ZipFileParallel(zipfile.ZipFile):
         zinfo.file_size = len(data)  # Uncompressed size
         crc = zipfile.crc32(data, 0)
         # compress data
-        compressor = zipfile._get_compressor(
-            zinfo.compress_type, zinfo._compresslevel)
-        if compressor and hasattr(compressor, 'compress'):
+        compressor = zipfile._get_compressor(zinfo.compress_type, zinfo._compresslevel)
+        if compressor and hasattr(compressor, "compress"):
             data = compressor.compress(data)
             data += compressor.flush()
 
         with self._lock:
-            with self.open(zinfo, mode='w') as dest:
+            with self.open(zinfo, mode="w") as dest:
                 dest._compressor = None  # remove the compressor so it doesn't compress again
                 dest.write(data)
                 dest._crc = crc
@@ -127,21 +126,21 @@ class _SH_OT_ExportLibrary(Operator):
         name="Type",
         description="The type of compression to use when zipping the pack",
         items=comp_enum,
-        default="ZIP_BZIP2"
+        default="ZIP_BZIP2",
     )
     deflated_compression_level: IntProperty(
         name="Level",
         description="A value of 1 (Best Speed) is fastest and produces the least compression, while a value of 9 (Best Compression) is slowest and produces the most. 0 is no compression. The default value is 6 which is a good compromise between speed and compression.",
         min=0,
         max=9,
-        default=6
+        default=6,
     )
     bzip_compression_level: IntProperty(
         name="Level",
         description="The level of compression to use when using the bzip2 compression type. 1 produces the least compression, and 9 (default) produces the most compression",
         min=1,
         max=9,
-        default=9
+        default=9,
     )
 
     @classmethod
@@ -149,7 +148,7 @@ class _SH_OT_ExportLibrary(Operator):
         if not polls.is_asset_browser(context, cls=cls):
             return False
 
-        scene_sets: 'scene.SH_Scene' = context.scene.superhive
+        scene_sets: "scene.SH_Scene" = context.scene.superhive
         if scene_sets.library_mode != "SUPERHIVE":
             cls.poll_message_set("Library mode must be 'SUPERHIVE'")
             return False
@@ -203,9 +202,12 @@ class _SH_OT_ExportLibrary(Operator):
         self.prog = scene_sets.header_progress_bar
         self.zip_path = Path(f"{self.lib.path}.zip")
         self.z = ZipFileParallel(
-            self.zip_path, "w",
+            self.zip_path,
+            "w",
             getattr(zipfile, self.compression_type),
-            compresslevel=self.deflated_compression_level if self.compression_type == "ZIP_DEFLATED" else self.bzip_compression_level
+            compresslevel=self.deflated_compression_level
+            if self.compression_type == "ZIP_DEFLATED"
+            else self.bzip_compression_level,
         )
 
         # Setup export thread
@@ -275,7 +277,7 @@ class _SH_OT_ExportLibrary(Operator):
         self.z.writestr(str(file.relative_to(self.directory)), file.read_bytes())
         self.files_written += 1
         self.updated = True
-        print(f"{(self.files_written/self.total_files)*100:.2f}%")
+        print(f"{(self.files_written / self.total_files) * 100:.2f}%")
 
 
 class SH_OT_ExportLibrary(Operator):
@@ -289,7 +291,7 @@ class SH_OT_ExportLibrary(Operator):
     total_files = 1
     files_written = 0
     updated = False
-    
+
     movingfiles_main_prog = 0
     movingfiles_sub_prog = 0
     movingfiles_sub_show = False
@@ -297,35 +299,35 @@ class SH_OT_ExportLibrary(Operator):
     file_label = ""
     sub_label = ""
     zipping_bar_prog = 0
-    
+
     is_zipping = False
 
     compression_type: EnumProperty(
         name="Type",
         description="The type of compression to use when zipping the pack",
         items=comp_enum,
-        default="ZIP_BZIP2"
+        default="ZIP_BZIP2",
     )
     deflated_compression_level: IntProperty(
         name="Level",
         description="A value of 1 (Best Speed) is fastest and produces the least compression, while a value of 9 (Best Compression) is slowest and produces the most. 0 is no compression. The default value is 6 which is a good compromise between speed and compression.",
         min=0,
         max=9,
-        default=6
+        default=6,
     )
     bzip_compression_level: IntProperty(
         name="Level",
         description="The level of compression to use when using the bzip2 compression type. 1 produces the least compression, and 9 (default) produces the most compression",
         min=1,
         max=9,
-        default=9
+        default=9,
     )
 
     selected_only: BoolProperty(
         name="Selected Only",
         description="Only export selected assets",
     )
-    
+
     target_zipfile_size: FloatProperty(
         name="Target Size (GBs)",
         description="The target max size each zip file should be cut off at and a new file started in Gigabytes. Would advise going under as each asset and its files will not be split up among zip files",
@@ -373,14 +375,14 @@ class SH_OT_ExportLibrary(Operator):
             layout.prop(self, "deflated_compression_level")
         elif self.compression_type == "ZIP_BZIP2":
             layout.prop(self, "bzip_compression_level")
-        
+
         layout.prop(self, "target_zipfile_size")
-    
+
     def invoke(self, context, event):
         self.z_to_close = []
         context.window_manager.invoke_props_dialog(self, width=400)
         return {"RUNNING_MODAL"}
-    
+
     def execute(self, context):
         print()
         print()
@@ -392,56 +394,57 @@ class SH_OT_ExportLibrary(Operator):
         else:
             self.lib = utils.from_active(context, area=context.area, load_assets=True)
             self.assets: list[AssetRepresentation] = self.lib.get_possible_assets()
-            
+
         self.zip_path = self.lib.path / f"{self.lib.name}.zip"
-        
+
         self._target_file_size = self.target_zipfile_size * 1_000_000_000
-        
-        scn_sets: 'scene.SH_Scene' = context.scene.superhive
+
+        scn_sets: "scene.SH_Scene" = context.scene.superhive
         self.prog = scn_sets.export_library
         context.window_manager.modal_handler_add(self)
         self._timer = context.window_manager.event_timer_add(0.01, window=context.window)
-        
+
         self._th = Thread(target=self.handle_files)
-        
+
         self._th.start()
         self.prog.start()
-        
+
         return {"RUNNING_MODAL"}
-    
+
     def handle_files(self):
         with tempfile.TemporaryDirectory() as tempdir:
             tempdir = Path(tempdir)
             print("Moving files to tempdir...")
             print("Progress:   0.00%", end="\r")
             self.movingfiles_main_prog = 0
-            self.updated=True
+            self.updated = True
             self.prog.movingfiles_bar.main.update_start_time()
             self.prog.movingfiles_bar.show_sub = True
-            for i,asset in enumerate(self.assets):
+            for i, asset in enumerate(self.assets):
                 p = Path(asset.full_library_path)
-                
+
                 self.file_label = p.name
                 self.prog.movingfiles_bar.file.update_start_time()
                 self.movingfiles_file_prog = 0
-                self.updated=True
-                
-                utils.export_helper(p, tempdir, op=self)
-                
-                self.movingfiles_main_prog = i/len(self.assets)
                 self.updated = True
-                
-                prog = f"{self.movingfiles_main_prog*100:.2f}"
+
+                utils.export_helper(p, tempdir, op=self)
+
+                self.movingfiles_main_prog = i / len(self.assets)
+                self.updated = True
+
+                prog = f"{self.movingfiles_main_prog * 100:.2f}"
                 print(f"Progress: {prog.rjust(6)}%", end="\r")
             self.movingfiles_main_prog = 1
             self.updated = True
             print("Progress: 100.00%")
             print()
-                
-            
-            files_to_write: list[tuple[Path, list[Path]]] = list((d, [f for f in d.rglob("*") if f.is_file()]) for d in tempdir.iterdir() )
+
+            files_to_write: list[tuple[Path, list[Path]]] = list(
+                (d, [f for f in d.rglob("*") if f.is_file()]) for d in tempdir.iterdir()
+            )
             """A list of tuples containing the directory and a list of files in that directory"""
-            
+
             total_files = sum(len(files) for _, files in files_to_write)
             self.is_zipping = True
             # zip_path = tempdir / "export.zip"
@@ -460,17 +463,17 @@ class SH_OT_ExportLibrary(Operator):
                         for file in files:
                             zipf.write(file, file.relative_to(tempdir))
                             files_zipped += 1
-                            
-                            self.zipping_bar_prog = files_zipped/total_files
+
+                            self.zipping_bar_prog = files_zipped / total_files
                             self.updated = True
-                            
-                            prog = f"{self.zipping_bar_prog*100:.2f}"
+
+                            prog = f"{self.zipping_bar_prog * 100:.2f}"
                             print(f"Progress: {prog.rjust(6)}%", end="\r")
-                            
+
                         if zipf.fp.tell() > self._target_file_size:
                             is_multi = True
                             break
-                        
+
             if is_multi:
                 self.zip_path = self.zip_path.rename(self.zip_path.with_stem(f"{self.zip_path.stem}_{self.zip_num}"))
             self.zipping_bar_prog = 1
@@ -479,12 +482,12 @@ class SH_OT_ExportLibrary(Operator):
 
     def modal(self, context, event):
         context.area.tag_redraw()
-        
+
         if self.is_zipping:
             self.prog.zipping_bar.update_formated_time()
         else:
             self.prog.movingfiles_bar.update_formated_time()
-        
+
         if self.updated:
             self.updated = False
             self.prog.movingfiles_bar.main.progress = self.movingfiles_main_prog
@@ -492,18 +495,18 @@ class SH_OT_ExportLibrary(Operator):
             self.prog.movingfiles_bar.file_label = self.file_label
             self.prog.zipping_bar.progress = self.zipping_bar_prog
             # self.prog.upload_bar.progress = self.upload_bar_prog
-            
+
             if self.movingfiles_sub_show:
                 self.prog.movingfiles_bar.show_sub = True
                 self.prog.movingfiles_bar.sub_label = self.sub_label
                 self.prog.movingfiles_bar.sub.progress = self.movingfiles_sub_prog
             else:
                 self.prog.movingfiles_bar.show_sub = False
-        
+
         if not self._th.is_alive():
             self.finished(context)
             return {"FINISHED"}
-        
+
         return {"RUNNING_MODAL"}
 
     def finished(self, context: Context) -> None:
@@ -517,16 +520,14 @@ class SH_OT_ExportLibrary(Operator):
         print()
         print()
         print()
-    
+
     def delayed_close(self):
         self.prog.end()
         for area in bpy.context.screen.areas:
             area.tag_redraw()
-    
 
-classes = (
-    SH_OT_ExportLibrary,
-)
+
+classes = (SH_OT_ExportLibrary,)
 
 
 def register():
--- stand_alone_scripts/export_helper.py
+++ stand_alone_scripts/export_helper.py
@@ -13,10 +13,94 @@ Argument Order:
 
 import sys
 from pathlib import Path
+from typing import Any
 
 import bpy
+import numpy as np
+import OpenImageIO.OpenImageIO as oiio
+from OpenImageIO import ROI, ImageBuf, ImageSpec
 
 DST = sys.argv[6]
+
+
+def set_compression(spec: ImageSpec, extension: str, quality: int = 100, quality_other: Any = None) -> None:
+    if extension == ".bmp":
+        spec.attribute("compression", quality_other)  # "rle4" or "rle8"
+    elif extension in {".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi"}:
+        spec.attribute("Compression", f"jpeg:{quality}")
+    elif extension == ".exr":
+        # quality_other= "none", "rle", "zips", "zip", "piz", "pxr24", "b44", "b44a", "dwaa", "dwab"
+        spec.attribute("compression", quality_other)
+    elif extension == ".png":
+        # quality needs to be between 0 and 9
+        quality = quality / 100 * 9
+        spec.attribute("png:compressionLevel", quality)
+    elif extension in {".tif", ".tiff", ".tx", ".env", ".sm", ".vsm"}:
+        # quality_other= "none", "lzw", "zip", "ccittrle", "jpeg", "backbits"
+        spec.attribute("compression", quality_other)
+        spec.attribute("tiff:compression", quality)
+
+
+def numpy_to_image(
+    buf: np.ndarray,
+    save_path: Path | str = None,
+    quality: int = 100,
+    quality_other: Any = None,
+) -> ImageBuf:
+    """
+    Convert a numpy array to an image.
+
+    Parameters
+    ----------
+    buf : np.ndarray
+        The numpy array to convert.
+    path_path : Path or str
+        The path to the image file to write. If not path is supplied the image will not be saved.
+    quality : int, optional
+        The quality of the image to save. Only applies if `save_path` is provided.
+
+    Returns
+    -------
+    ImageBuf
+        The image.
+
+    """
+    image = ImageBuf(ImageSpec(buf.shape[1], buf.shape[0], buf.shape[2], oiio.FLOAT))
+    image.set_pixels(ROI(0, buf.shape[1], 0, buf.shape[0]), buf)
+    if save_path is not None:
+        set_compression(
+            image.spec(),
+            Path(save_path).suffix,
+            quality=quality,
+            quality_other=quality_other,
+        )
+        save_path.parent.mkdir(parents=True, exist_ok=True)
+        image.write(str(save_path))
+        if image.has_error:
+            error = image.geterror()
+            print("Error saving image:", error)
+            raise Exception(error)
+    return image
+
+
+def save_out_preview(item: bpy.types.ID, directory: Path):
+    if item.asset_data and item.preview:
+        pixels = np.array(item.preview.image_pixels_float)
+
+        d3 = 4
+        if len(pixels) == item.preview.image_size[0] * item.preview.image_size[1] * 3:
+            d3 = 3
+        pixels.resize(list(item.preview.image_size) + [d3])
+        fp = (
+            directory
+            / "Thumbnails"
+            / f"{Path(bpy.data.filepath).stem}_{item.__class__.__name__.lower()}_{item.name}_preview.webp"
+        )
+        print(f"Saving preview for {item.name} to {fp}")
+        numpy_to_image(
+            pixels[::-1],
+            save_path=fp,
+        )
 
 
 if __name__ == "__main__":
@@ -30,9 +114,7 @@ if __name__ == "__main__":
     def make_rel(path: Path) -> Path:
         return path.relative_to(d)
 
-    images_to_copy = [
-        img for img in bpy.data.images if not img.packed_file and not img.packed_files
-    ]
+    images_to_copy = [img for img in bpy.data.images if not img.packed_file and not img.packed_files]
     sounds_to_copy = [sound for sound in bpy.data.sounds if not sound.packed_file]
     volumes_to_copy = [volume for volume in bpy.data.volumes if not volume.packed_file]
 
@@ -47,7 +129,7 @@ if __name__ == "__main__":
             print("|            Sequence:   0.00%+=+r")
             print("=sub_label=Sequence")
             print("=sub_show=True")
-            print(f"=sub_prog=0")
+            print("=sub_prog=0")
             seq_prog = str(0)
             base_filepath = Path(img.filepath)
             vol_dir = base_filepath.parent
@@ -59,21 +141,15 @@ if __name__ == "__main__":
             files_to_copy = [
                 f
                 for f in vol_dir.iterdir()
-                if f.is_file()
-                and f.name.startswith(name_base)
-                and f.suffix == base_filepath.suffix
+                if f.is_file() and f.name.startswith(name_base) and f.suffix == base_filepath.suffix
             ]
             for j, f in enumerate(files_to_copy):
-                if (
-                    f.is_file()
-                    and f.name.startswith(name_base)
-                    and f.suffix == base_filepath.suffix
-                ):
+                if f.is_file() and f.name.startswith(name_base) and f.suffix == base_filepath.suffix:
                     dst = d / f.name
                     dst.write_bytes(f.read_bytes())
                     sub_prog = j / len(files_to_copy)
                     print(f"=sub_prog={sub_prog}")
-                    seq_prog = f"{sub_prog*100:.2f}"
+                    seq_prog = f"{sub_prog * 100:.2f}"
                     print(f"|        Progress: {seq_prog.rjust(6)}%+=+r")
             print("=sub_show=False")
         else:
@@ -84,7 +160,7 @@ if __name__ == "__main__":
                 img.filepath = str(make_rel(dst))
         prg = i / total_assets
         print(f"=prog={prg}")
-        prog = f"{i/len(bpy.data.images)*100:.2f}"
+        prog = f"{i / len(bpy.data.images) * 100:.2f}"
         print(f"|        Progress: {prog.rjust(6)}%+=+r")
     print("|        Progress: 100.00%")
     print("|")
@@ -104,7 +180,7 @@ if __name__ == "__main__":
             sound.filepath = str(make_rel(dst))
         prg = (i + assets_moved) / total_assets
         print(f"=prog={prg}")
-        prog = f"{i/len(bpy.data.sounds)*100:.2f}"
+        prog = f"{i / len(bpy.data.sounds) * 100:.2f}"
         print(f"|        Progress: {prog.rjust(6)}%+=+r")
     print("|")
 
@@ -130,23 +206,17 @@ if __name__ == "__main__":
             files_to_copy = [
                 f
                 for f in vol_dir.iterdir()
-                if f.is_file()
-                and f.name.startswith(name_base)
-                and f.suffix == base_filepath.suffix
+                if f.is_file() and f.name.startswith(name_base) and f.suffix == base_filepath.suffix
             ]
             seq_prog = "0.00"
-            print(f"=sub_prog=0")
+            print("=sub_prog=0")
             for j, f in enumerate(files_to_copy):
-                if (
-                    f.is_file()
-                    and f.name.startswith(name_base)
-                    and f.suffix == base_filepath.suffix
-                ):
+                if f.is_file() and f.name.startswith(name_base) and f.suffix == base_filepath.suffix:
                     dst = d / f.name
                     dst.write_bytes(f.read_bytes())
                     sub_prog = j / len(files_to_copy)
                     print(f"=sub_prog={sub_prog}")
-                    seq_prog = f"{sub_prog*100:.2f}"
+                    seq_prog = f"{sub_prog * 100:.2f}"
                     print(f"|        Progress: {seq_prog.rjust(6)}%+=+r")
             print("=sub_show=False")
             print("|            Sequence: 100.00%")
@@ -158,7 +228,21 @@ if __name__ == "__main__":
                 volume.filepath = str(make_rel(dst))
             prg = (i + assets_moved) / total_assets
             print(f"=prog={prg}")
-            prog = f"{i/len(bpy.data.volumes)*100:.2f}"
+            prog = f"{i / len(bpy.data.volumes) * 100:.2f}"
             print(f"|        Progress: {prog.rjust(6)}%+=+r")
+
+    # Export thumbnails of all assets
+    for action in bpy.data.actions:
+        save_out_preview(action, d)
+    for collection in bpy.data.collections:
+        save_out_preview(collection, d)
+    for material in bpy.data.materials:
+        save_out_preview(material, d)
+    for node_tree in bpy.data.node_groups:
+        save_out_preview(node_tree, d)
+    for obj in bpy.data.objects:
+        save_out_preview(obj, d)
+    for world in bpy.data.worlds:
+        save_out_preview(world, d)
 
     bpy.ops.wm.save_mainfile(filepath=str(d / blend_path.name), compress=True)
